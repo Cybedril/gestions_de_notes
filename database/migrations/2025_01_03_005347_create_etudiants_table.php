e6bd0fbb57e18826a79a360148ffd7bbb79cba7c
Besilia-1 <ahomagnonbesilia1@gmail.com>
2025-01-03 22:30:48 +0100

    des creation des tables

--- database/migrations/2025_01_03_005347_create_etudiants_table.php
+++ database/migrations/2025_01_03_005347_create_etudiants_table.php
@@ -6,20 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    
+    public function up()
     {
         Schema::create('etudiants', function (Blueprint $table) {
             $table->id();
+            $table->string('numero_etudiant')->unique();
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('niveau'); // L1, L2, L3
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+    
+    
     public function down(): void
     {
         Schema::dropIfExists('etudiants');
