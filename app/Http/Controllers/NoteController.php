2bb5be9ca9b1f425367b27ca3f250e2c879ad8bc
Besilia-1 <ahomagnonbesilia1@gmail.com>
2025-01-03 13:45:25 +0100

    [Notes] calcul des moyennes

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
+use App\Models\Etudiant;
+use App\Models\ElementConstitutif;
 use Illuminate\Http\Request;
 
 class NoteController extends Controller
 {
-    //
+    public function index()
+    {
+        $notes = Note::with('etudiant', 'ec')->get();
+        return view('notes.index', compact('notes'));
+    }
+
+    public function create()
+    {
+        $etudiants = Etudiant::all();
+        $ecs = EC::all();
+    
+        return view('notes.create', compact('etudiants', 'ecs'));
+    }
+    
+    public function store(Request $request)
+    {
+        $request->validate([
+            'etudiant_id' => 'required|exists:etudiants,id',
+            'ec_id' => 'required|exists:elements_constitutifs,id',
+            'note' => 'required|numeric|min:0|max:20',
+            'session' => 'required|in:normale,rattrapage',
+        ]);
+    
+        Note::create($request->all());
+    
+        return redirect()->route('notes.index')->with('success', 'Note enregistrée avec succès !');
+    }
+    public function results()
+{
+    $etudiants = Etudiant::all();
+    return view('notes.results', compact('etudiants'));
+}
+
+
 }
